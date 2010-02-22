f268f1a1ef3eac994c1011ac36fc23d66746e651
0101ds <0101ds@8f0a55fe-1f8f-11df-90ab-373db8be4c2b>
2010-02-22 10:35:24 +0000

    add test case
    git-svn-id: http://milui.googlecode.com/svn/trunk@3 8f0a55fe-1f8f-11df-90ab-373db8be4c2b

---
+++ test/object.c
@@ -0,0 +1,15 @@
+/*! ============================================================================
+ * @file object.c 
+ * @Synopsis  
+ * @author DongKai
+ * @version 1.0
+ * @date 2010年02月22日
+ *  Company: Beijing Feynman Software Technology Co., Ltd.
+ */
+
+#include <milui/cobject.h>
+
+int main()
+{
+
+}
