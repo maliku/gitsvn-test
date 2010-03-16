b0de697eeb0761b5cdc7067f771dae2ea4494866
0101ds <0101ds@8f0a55fe-1f8f-11df-90ab-373db8be4c2b>
2010-03-16 02:53:58 +0000

    add video layout file tree.
    git-svn-id: http://milui.googlecode.com/svn/trunk@34 8f0a55fe-1f8f-11df-90ab-373db8be4c2b

---
+++ src/video/dummy/DummyVideo.h
@@ -0,0 +1,13 @@
+/*!============================================================================
+ * @file DummyVideo.h 
+ * @Synopsis  
+ * @author DongKai
+ * @version 1.0
+ * @date 2010年03月16日 10时34分05秒 
+ *  Company: Beijing Feynman Software Technology Co., Ltd.
+ */
+
+#ifndef  DUMMYVIDEO_INC
+#define  DUMMYVIDEO_INC
+
+#endif   /* ----- #ifndef DUMMYVIDEO_INC  ----- */
---
+++ src/video/video.c
@@ -0,0 +1,10 @@
+/*! ============================================================================
+ * @file video.c 
+ * @Synopsis  
+ * @author DongKai
+ * @version 1.0
+ * @date 2010年03月16日
+ *  Company: Beijing Feynman Software Technology Co., Ltd.
+ */
+
+
