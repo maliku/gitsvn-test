8b913eee0eea792e760ab0a61ea39cd3fe56c8ea
0101ds <0101ds@8f0a55fe-1f8f-11df-90ab-373db8be4c2b>
2010-03-24 13:37:30 +0000

    add some file for dummy video device.
    git-svn-id: http://milui.googlecode.com/svn/trunk@37 8f0a55fe-1f8f-11df-90ab-373db8be4c2b

--- src/include/video_device.h
+++ src/include/video_device.h
@@ -11,22 +11,145 @@
 #define  GFX_INC
 
 #include "coo.h"
-#include "MIL_surface.h"
+#include "MIL_video.h"
 
 CLASS(VideoDevice) {
     VIRTUAL_METHOD_DECLARE_BEGIN(VideoDevice) 
-        int (*VideoInitialize)(_Self(VideoDevice));
-        MIL_Rect** (*VideoModeList)(_Self(VideoDevice));
-        MIL_Surface* (*SetVideoMode)(_Self(VideoDevice));
-        int (*ToggleFullScreen)(_Self(VideoDevice));
-        void (*VideoQuit)(_Self(VideoDevice));
-        void (*UpdateRects)(_Self(VideoDevice), int numrects, MIL_Rect *rects);
+	/* * * */
+	/* Initialization/Query functions */
+
+	/* Initialize the native video subsystem, filling 'vformat' with the 
+	   "best" display pixel format, returning 0 or -1 if there's an error.
+	 */
+	int (*videoInit)(_Self(VideoDevice), MIL_PixelFormat *vformat);
+
+	/* List the available video modes for the given pixel format, sorted
+	   from largest to smallest.
+	 */
+	MIL_Rect **(*listModes)(_Self(VideoDevice), MIL_PixelFormat *format, Uint32 flags);
+
+	/* Set the requested video mode, returning a surface which will be
+	   set to the MIL_VideoSurface.  The width and height will already
+	   be verified by ListModes(), and the video subsystem is free to
+	   set the mode to a supported bit depth different from the one
+	   specified -- the desired bpp will be emulated with a shadow
+	   surface if necessary.  If a new mode is returned, this function
+	   should take care of cleaning up the current mode.
+	 */
+	MIL_Surface *(*setVideoMode)(_Self(VideoDevice), MIL_Surface *current,
+				int width, int height, int bpp, Uint32 flags);
+
+	/* Toggle the fullscreen mode */
+	int (*toggleFullScreen)(_Self(VideoDevice), int on);
+
+	/* This is called after the video mode has been set, to get the
+	   initial mouse state.  It should queue events as necessary to
+	   properly represent the current mouse focus and position.
+	 */
+	void (*updateMouse)(_Self(VideoDevice));
+
+	/* Create a YUV video surface (possibly overlay) of the given
+	   format.  The hardware should be able to perform at least 2x
+	   scaling on display.
+	 */
+	MIL_Overlay *(*createYUVOverlay)(_Self(VideoDevice), int width, int height,
+	                                 Uint32 format, MIL_Surface *display);
+
+        /* Sets the color entries { firstcolor .. (firstcolor+ncolors-1) }
+	   of the physical palette to those in 'colors'. If the device is
+	   using a software palette (MIL_HWPALETTE not set), then the
+	   changes are reflected in the logical palette of the screen
+	   as well.
+	   The return value is 1 if all entries could be set properly
+	   or 0 otherwise.
+	*/
+	int (*setColors)(_Self(VideoDevice), int firstcolor, int ncolors,
+			 MIL_Color *colors);
+
+	/* This pointer should exist in the native video subsystem and should
+	   point to an appropriate update function for the current video mode
+	 */
+	void (*updateRects)(_Self(VideoDevice), int numrects, MIL_Rect *rects);
+
+	/* Reverse the effects VideoInit() -- called if VideoInit() fails
+	   or if the application is shutting down the video subsystem.
+	*/
+	void (*videoQuit)(_Self(VideoDevice));
+
+	/* * * */
+	/* Hardware acceleration functions */
+
+	/* Allocates a surface in video memory */
+	int (*allocHWSurface)(_Self(VideoDevice), MIL_Surface *surface);
+
+	/* Sets the hardware accelerated blit function, if any, based
+	   on the current flags of the surface (colorkey, alpha, etc.)
+	 */
+	int (*checkHWBlit)(_Self(VideoDevice), MIL_Surface *src, MIL_Surface *dst);
+
+	/* Fills a surface rectangle with the given color */
+	int (*fillHWRect)(_Self(VideoDevice), MIL_Surface *dst, MIL_Rect *rect, Uint32 color);
+
+	/* Sets video mem colorkey and accelerated blit function */
+	int (*setHWColorKey)(_Self(VideoDevice), MIL_Surface *surface, Uint32 key);
+
+	/* Sets per surface hardware alpha value */
+	int (*setHWAlpha)(_Self(VideoDevice), MIL_Surface *surface, Uint8 value);
+
+	/* Returns a readable/writable surface */
+	int (*lockHWSurface)(_Self(VideoDevice), MIL_Surface *surface);
+	void (*unlockHWSurface)(_Self(VideoDevice), MIL_Surface *surface);
+
+	/* Performs hardware flipping */
+	int (*flipHWSurface)(_Self(VideoDevice), MIL_Surface *surface);
+
+	/* Frees a previously allocated video surface */
+	void (*freeHWSurface)(_Self(VideoDevice), MIL_Surface *surface);
+
+	/* Set the gamma correction directly (emulated with gamma ramps) */
+	int (*SetGamma)(_Self(VideoDevice), float red, float green, float blue);
+
+	/* Get the gamma correction directly (emulated with gamma ramps) */
+	int (*GetGamma)(_Self(VideoDevice), float *red, float *green, float *blue);
+
+	/* Set the gamma ramp */
+	int (*SetGammaRamp)(_Self(VideoDevice), Uint16 *ramp);
+
+	/* Get the gamma ramp */
+	int (*GetGammaRamp)(_Self(VideoDevice), Uint16 *ramp);
+
+	/* * * */
+	/* OpenGL support */
+
+	/* Sets the dll to use for OpenGL and loads it */
+	int (*GL_LoadLibrary)(_Self(VideoDevice), const char *path);
+
+	/* Retrieves the address of a function in the gl library */
+	void* (*GL_GetProcAddress)(_Self(VideoDevice), const char *proc);
+
+	/* Get attribute information from the windowing system. */
+	int (*GL_GetAttribute)(_Self(VideoDevice), MIL_GLattr attrib, int* value);
+
+	/* Make the context associated with this driver current */
+	int (*GL_MakeCurrent)(_Self(VideoDevice));
+
+	/* Swap the current buffers in double buffer mode. */
+	void (*GL_SwapBuffers)(_Self(VideoDevice));
+
 
     VIRTUAL_METHOD_DECLARE_END 
 
-        METHOD_DECLARE_PLACEHOLDER(VideoDevice)
+    METHOD_DECLARE_BEGIN(VideoDevice)
+        char* (*getName)(_Self(VideoDevice), char* buff, Uint32 len);
+    METHOD_DECLARE_END
 
-        const char *name;
+    const char *name;
+	/* Information about the video hardware */
+	MIL_VideoInfo info;
+
+	/* The pixel format used when MIL_CreateRGBSurface creates MIL_HWSURFACEs with alpha */
+	MIL_PixelFormat* displayformatalphapixel;
+	
 };
 
 #endif   /* ----- #ifndef GFX_INC  ----- */
---
+++ src/video/dummy/dummy_device.c
@@ -0,0 +1,11 @@
+/*! ============================================================================
+ * @file dummy_device.c 
+ * @Synopsis  
+ * @author DongKai
+ * @version 1.0
+ * @date 2010年03月24日
+ *  Company: Beijing Feynman Software Technology Co., Ltd.
+ */
+
+#include "video_device.h"
+
