d9cae6a6eb02e17f146989b6f8b5aea5ef50f95f
0101ds <0101ds@8f0a55fe-1f8f-11df-90ab-373db8be4c2b>
2010-04-12 10:29:20 +0000

    git-svn-id: http://milui.googlecode.com/svn/trunk@42 8f0a55fe-1f8f-11df-90ab-373db8be4c2b

--- src/video/surface.c
+++ src/video/surface.c
@@ -9,6 +9,9 @@
 
 #include "surface.h"
 
+MAKE_PURE_VIRTUAL_CLASS(MIL_Surface)
+METHOD_REGISTER_PLACEHOLDER(MIL_Surface)
+
 CONSTRUCTOR(Surface)
 {
     return self;
@@ -17,3 +20,40 @@ CONSTRUCTOR(Surface)
 DESTRUCTOR(Surface)
 {
 }
+
+int  METHOD_NAMED(Surface, lock)(_Self(MIL_Surface))
+{}
+
+void METHOD_NAMED(Surface, unlock)(_Self(MIL_Surface))
+{}
+
+int  METHOD_NAMED(Surface, setColorKey)(_Self(MIL_Surface), Uint32 flag, Uint32 key)
+{}
+
+int  METHOD_NAMED(Surface, setAlpha)(_Self(MIL_Surface), Uint32 flag, Uint8 alpha)
+{}
+
+void METHOD_NAMED(Surface, setClipRect)(_Self(MIL_Surface), MIL_Rect *rect)
+{}
+
+void METHOD_NAMED(Surface, getClipRect)(_Self(MIL_Surface), MIL_Rect *rect)
+{}
+
+int  METHOD_NAMED(Surface, blitSurface)(_Self(MIL_Surface), MIL_Rect *srcrect, Surface *dst, MIL_Rect *dstrect)
+{}
+
+int  METHOD_NAMED(Surface, fillRect)(_Self(MIL_Surface), MIL_Rect *dstrect, Uint32 color)
+{}
+
+int  METHOD_NAMED(Surface, saveBMP)(_Self(MIL_Surface), const char *file)
+{}
+
+Surface* METHOD_NAMED(Surface, displayFormat)(_Self(MIL_Surface))
+{}
+
+Surface* METHOD_NAMED(Surface, displayFormatAlpha)(_Self(MIL_Surface))
+{}
+
+Surface* METHOD_NAMED(Surface, convert)(_Self(MIL_Surface), MIL_PixelFormat *fmt, Uint32 flags)
+{}
+
